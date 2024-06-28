cbac49338f49c33b2b81421d28f379ce4aa248a4
Rishav Raj <101454458+starsailor2@users.noreply.github.com>
2024-06-28 10:10:45 +0530

    Delete colpro directory

--- colpro/firstapp/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-
-# localhost:8000/firstapp
-# localhost:8000/firstapp/order
-urlpatterns = [
-    path("", views.all_first, name="all_firstapp"),
-]
--- colpro/firstapp/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def all_first(request):
-    return render(request, 'firstapp/all_first.html')
